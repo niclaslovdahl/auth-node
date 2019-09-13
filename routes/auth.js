47a668e523d08dc0a2c72bf0304a4d86419a6306
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-09-14 01:17:19 +0200

    Checks if email exist in db.
    If email exists return function with error code 400.

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,9 @@ router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
