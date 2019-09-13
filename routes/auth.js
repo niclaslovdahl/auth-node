7d6ac4ba9dd1bcaebf28abe342ed9f0db3953a48
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-09-13 19:21:53 +0200

    Added validation to registrer
    Validation using Joi on /registrer. validation.js created to separate
    valdation using validation functions.

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
   });
+
   try {
     const savedUser = await user.save();
     res.send(savedUser);
